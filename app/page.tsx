d95212968fe61beaeb40a754bbd317b3bb0e3db5
Your Name <you@example.com>
2023-09-01 10:40:25 +0700

    joy

--- app/page.tsx
+++ app/page.tsx
@@ -114,12 +114,12 @@ export default function Home() {
 
         <section
           id="about"
-          className="pt-32 pb-32 md:pt-20 md:pb-20 lg:pt-20 lg:pb-20 "
+          className="pt-20 pb-20 md:pt-20 md:pb-20 lg:pt-20 lg:pb-20"
         >
           <div className="container">
             <div className="flex flex-wrap">
               <div className="w-full px-4 mb-10 lg:w-1/2">
-                <h4 className="font-bold uppercase text-primary text-lg">
+                <h4 className="font-bold uppercase text-primary text-lg lg:text-2xl">
                   About Me
                 </h4>
                 <h2 className="font-bold text-dark text-3xl mb-5 max-w-xl lg:text-4xl">
@@ -132,33 +132,12 @@ export default function Home() {
               </div>
               <div className="w-full px-4 lg:w-1/2">
                 <h3 className="font-semibold text-dark text-2xl mb-4 lg:text-3xl lg:pt-10">
-                  Mari berteman
+                  Let`s be friend
                 </h3>
                 <p className="font-medium text-base text-secondary mb-6 lg:text-lg ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Voluptates libero aspernatur ab neque tempore quia soluta
-                  repellat at?
+                  Don`t forget to follow my social media to know more about me
                 </p>
-                <div className="text-5xl flex justify-center md:justify-start gap-16 text-gray-600 dark:text-gray-400">
-                  <Link
-                    href="https://www.instagram.com/kenan_bukit/"
-                    target="_blank"
-                  >
-                    <AiFillInstagram />
-                  </Link>
-                  <Link
-                    href="https://www.linkedin.com/in/kenan-tomfie0604"
-                    target="_blank"
-                  >
-                    <AiFillLinkedin />
-                  </Link>
-                  <Link
-                    href="https://youtube.com/@kenantomfiebukit989"
-                    target="_blank"
-                  >
-                    <AiFillYoutube />
-                  </Link>
-                </div>
+                
               </div>
             </div>
           </div>
